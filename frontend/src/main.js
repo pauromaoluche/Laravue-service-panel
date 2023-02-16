083e7a0fcbb2d65789253fb508a57768e1260af7
Pedro Pauluci <pedro.pauluci@elitesoft.com.br>
2023-02-16 18:07:47 -0300

    Update: export axios global

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,5 +4,17 @@ import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'http://127.0.0.1:8000/api/',
+    headers:
+    {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+});
+
+export default api
 
 createApp(App).use(router).mount('#app')
--- frontend/src/views/Login/LoginView.vue
+++ frontend/src/views/Login/LoginView.vue
@@ -1,23 +1,46 @@
 <template>
   <div class="loginBox"> <img class="user" src="https://i.ibb.co/yVGxFPR/2.png" height="100px" width="100px">
     <h3>Sign in here</h3>
-    <form action="login.php" method="post">
-      <div class="inputBox"> <input id="uname" type="text" name="Username" placeholder="Username"> <input id="pass"
-          type="password" name="Password" placeholder="Password"> </div> <input type="submit" name="" value="Login">
+    <form @submit.stop.prevent="submit">
+      <div class="inputBox">
+        <input v-model="email" id="uname" type="text" name="Username" placeholder="Username">
+        <input v-model="password" id="pass" type="password" name="Password" placeholder="Password">
+      </div>
+      <input type="submit" name="" value="Login">
     </form>
     <a href="#">Forget Password<br> </a>
     <div class="text-center">
       <p style="color: #59238F;">Sign-Up</p>
     </div>
 
-</div>
+  </div>
 </template>
   
 <script>
+
+import api from '@/main'
 export default {
 
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+
+  methods: {
+    submit() {
+      api.post('/login', {
+        email: this.email,
+        password: this.password
+      }).then((response) =>{
+        console.log(response.data);
+      }).catch((error) =>{
+        console.log(error);
+      })
+    }
+  }
 }
 </script>
 
-<style lang="scss" src="./style.scss" scoped>
-</style>
+<style lang="scss" src="./style.scss" scoped></style>
