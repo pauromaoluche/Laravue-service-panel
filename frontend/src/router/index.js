40455bd956134a3089f81468a24683098f60a9ea
Pedro Pauluci <pedro.pauluci@elitesoft.com.br>
2023-02-16 07:56:27 -0300

    Update: Removing some things

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,26 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
+  {
+    path: '/',
+    name: '',
+    component: () => import('../components/HelloWorld.vue')
+  },
   {
     path: '/login',
     name: 'login',
     component: () => import('../views/Login/LoginView.vue')
   },
-
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = createRouter({
