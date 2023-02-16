87a12602887a9adedb76e10630f400f5324f9618
Pedro Pauluci <pedro.pauluci@elitesoft.com.br>
2023-02-16 07:55:47 -0300

    Update: Login page

--- frontend/src/views/Login/LoginView.vue
+++ frontend/src/views/Login/LoginView.vue
@@ -1,6 +1,23 @@
 <template>
-    <div class="about">
-      <h1>testelogin</h1>
+  <div class="loginBox"> <img class="user" src="https://i.ibb.co/yVGxFPR/2.png" height="100px" width="100px">
+    <h3>Sign in here</h3>
+    <form action="login.php" method="post">
+      <div class="inputBox"> <input id="uname" type="text" name="Username" placeholder="Username"> <input id="pass"
+          type="password" name="Password" placeholder="Password"> </div> <input type="submit" name="" value="Login">
+    </form>
+    <a href="#">Forget Password<br> </a>
+    <div class="text-center">
+      <p style="color: #59238F;">Sign-Up</p>
     </div>
-  </template>
+
+</div>
+</template>
   
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" src="./style.scss" scoped>
+</style>
