968f5ea02b7d2100e668f3ae4442e77b19c396d4
Pedro Pauluci <pauluci125romao.pedro@hotmail.com>
2023-03-20 13:30:17 -0300

    Dynamic modal

---
+++ frontend/src/Components/ModalComponent.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="modal fade" :class="{ show: aberto, 'd-none': display, 'd-block': !display }"   tabindex="-1" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">{{ titulo }}</h1>
+                    <button type="button" class="btn-close" @click="fechar"></button>
+                </div>
+                <div class="modal-body">
+                    <slot></slot>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: "ModalComponent",
+    props: {
+        display: {
+            type: Boolean,
+            default: true,
+        },
+        aberto: {
+            type: Boolean,
+            default: false,
+        },
+        titulo: {
+            type: String,
+            default: '',
+        },
+    },
+    methods: {
+        fechar() {
+             this.$emit('func', false)
+        },
+    },
+};
+</script>
+  
+<style>
+.modal {
+    display: block;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+/* .modal-content {
+    background-color: white;
+    padding: 20px;
+} */
+</style>
+  
--- frontend/src/Components/TableComponent.vue
+++ frontend/src/Components/TableComponent.vue
@@ -4,36 +4,81 @@
             <thead>
                 <tr>
                     <th scope="col">Protocolo</th>
-                    <th scope="col">Cliente</th>
+                    <th width="100" scope="col">Cliente</th>
                     <th scope="col">Prioridade</th>
-                    <th scope="col">Criado em</th>
-                    <th scope="col">Data Prevista</th>
+                    <th width="100" scope="col">Criado</th>
+                    <th width="100" scope="col">Previsto</th>
                     <th scope="col">Descrição</th>
                     <th scope="col">Analista</th>
+                    <th style="text-align: center;" scope="col">Ações</th>
                 </tr>
             </thead>
             <tbody>
 
+                <button type="button" class="btn btn-primary" @click="abrirModal">Abrir modal</button>
+                <ModalComponent @func="fecharModal" v-model:display="display" :display="display" :aberto="modalAberto"
+                    titulo="Meu modal dinâmico">
+                    <p>Aqui vai o conteúdo do modal</p>
+                </ModalComponent>
                 <tr v-for="atend in data" :key="atend.id" class="table-sm">
                     <th scope="row">{{ atend.protocolo }}</th>
-                    <td>{{ atend.provedor.nome }}</td>
-                    <td>{{ atend.prioridade }}</td>
+                    <td class="name">{{ atend.provedor.nome }}</td>
+                    <td class="flag" v-if="atend.prioridade == 1">
+                        <span><font-awesome-icon icon="flag" color="green" /></span>
+                    </td>
+                    <td class="flag" v-else-if="atend.prioridade == 2">
+                        <span><font-awesome-icon icon="flag" color="orange" /></span>
+                    </td>
+                    <td class="flag" v-else-if="atend.prioridade == 3">
+                        <span><font-awesome-icon icon="flag" color="red" /></span>
+                    </td>
                     <td>{{ atend.created_at }}</td>
                     <td>{{ atend.data_prev }}</td>
-                    <td>{{ atend.desc }}</td>
+                    <td>{{ atend.desc.substring(0, 20) + ".." }}</td>
                     <td>{{ atend.user.name }}</td>
-                </tr>
 
+                    <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                        <button type="button" class="btn btn-outline-success">Ver</button>
+                        <button type="button" class="btn btn-outline-primary">Editar</button>
+                        <button type="button" class="btn btn-outline-danger">Excluir</button>
+                    </div>
+                </tr>
             </tbody>
         </table>
     </div>
 </template>
 <script>
-
+import ModalComponent from "./ModalComponent.vue"
 export default {
     name: "TableComponent",
+    components: {
+        ModalComponent
+    },
     props: {
         data: Array
+    },
+    data() {
+        return {
+            body: "teste",
+            modalAberto: false,
+            display: true,
+            onConfirm: () => {
+                console.log('Modal confirmed');
+            }
+        }
+    },
+    methods: {
+        fecharModal() {
+            setTimeout(() => this.modalAberto = false, 10);
+            setTimeout(() => this.display = true, 200);
+        },
+        abrirModal() {
+            this.display = false,
+                setTimeout(() => this.modalAberto = true, 100);
+        },
+    },
+    computed: {
+
     }
 }
 
@@ -47,7 +92,19 @@ export default {
 }
 
 td {
-    padding: 12px !important;
+    padding: 10px !important;
     font-size: 12px;
+}
+
+td.name {
+    text-align: center;
+}
+
+td.flag {
+    text-align: center;
+
+    span svg {
+        font-size: 20px;
+    }
 }
 </style>
