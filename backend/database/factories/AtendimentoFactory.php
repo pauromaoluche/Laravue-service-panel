f6ed5b21d3d981052ecd44d2e8b696a8fa339d2f
Pedro Pauluci <pauluci125romao.pedro@hotmail.com>
2023-06-26 09:55:18 -0300

    Add description and end date of scheduling

--- backend/database/factories/AtendimentoFactory.php
+++ backend/database/factories/AtendimentoFactory.php
@@ -22,6 +22,50 @@ class AtendimentoFactory extends Factory
         $dataFinal = clone $dataInicial;
         $dataFinal->add(new DateInterval("P{$intervaloDias}D"));
 
+
+        //Ira Verificar o status, para preencher a data_agend e a data_fim de acordo com a regra
+        $dataAgend = clone $dataInicial;
+        $interAgend = $this->faker->numberBetween(1, 20);
+        $dataAgend->add(new DateInterval("P{$interAgend}D"));
+
+        $dataFim = clone $dataAgend;
+        $interFim = $this->faker->numberBetween(1, 10);
+        $dataFim->add(new DateInterval("P{$interFim}D")); 
+
+        $status = rand(1, 4);
+
+        switch($status){
+            case 1:
+                // AG aguardando a gendamento
+                $DA= null;
+                $DF = null;
+                $descFim = null;
+                break;
+            case 2:
+                // A agendada
+                $DA = $dataAgend;
+                $DF = null;
+                $descFim = null;
+                break;
+            default:
+                // C concluida
+                // FS Fechada sem solução
+                $DA = $dataAgend;
+                $DF = $dataFim;
+                $descFim = $this->faker->sentence;
+                break;
+
+
+        }
+
+        if($status == 1){
+            
+            
+        }else if($status == 2){
+            
+            
+        }
+
         return [
             'gerado_por' => rand(1, 20),
             'provedor_id' => rand(1, 7),
@@ -33,7 +77,11 @@ class AtendimentoFactory extends Factory
             'desc' => $this->faker->sentence,
             'user_id' => rand(1, 20),
             'setor_id' => rand(1, 4),
-            'status_atend' => rand(1, 4)
+            'status_atend' => $status,
+            'desc_fim' => $descFim,
+            'data_agend' => $DA,
+            'data_fim' => $DF
+
         ];
     }
 }
